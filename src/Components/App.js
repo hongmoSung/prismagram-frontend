ed63f47434ebb994d84d92297d5e54f328c85732
hongmo sung <tesung30@naver.com>
2019-12-01 02:17:49 +0900

    Header Logic

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
-import Routers from './Routers';
+import Routes from './Routes';
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -35,7 +35,7 @@ export default () => {
           <>
             <Header />
             <Wrapper>
-              <Routers isLoggedIn={isLoggedIn} />
+              <Routes isLoggedIn={isLoggedIn} />
               <Footer />
             </Wrapper>
           </>
